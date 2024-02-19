2c21ba5471c513e011b3c373e932cc19a5c573cb
Mike Bishop <mike@drinklist.co>
2024-02-19 16:03:34 +0000

    PASS test_convert_five_degrees_twelve_minutes_eighteen_seconds_west

--- geophoto.py
+++ geophoto.py
@@ -2,4 +2,4 @@
 
 
 def dms_to_decimal(degrees, minutes, seconds, ref='N'):
-    return (degrees + minutes/60 + seconds/3600) * (-1 if ref == 'S' else 1)
+    return (degrees + minutes/60 + seconds/3600) * (-1 if ref == 'S' or ref == 'W' else 1)
