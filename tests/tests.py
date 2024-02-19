fa9cf17b7bc064b780cc305898e350003fadbe9c
Mike Bishop <mike@drinklist.co>
2024-02-19 15:47:02 +0000

    PASS test_convert_zero

--- tests/tests.py
+++ tests/tests.py
@@ -6,7 +6,7 @@ import geophoto
 
 class TestConvertDegreesMinutesSecondsToDecimal(unittest.TestCase):
 
-    def test_convert(self):
+    def test_convert_zero(self):
         test_decimal = 0
         self.assertEqual(test_decimal, geophoto.dms_to_decimal(0, 0, 0))
 
