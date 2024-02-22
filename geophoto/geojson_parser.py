1b3f182397b85bd28151066b10f306e3fbd2c94b
Mike Bishop <mike@drinklist.co>
2024-02-22 15:01:09 +0000

    ADD FeatureCollection to _collection_dictionary, ADD __iter__ to class, PASS test_geojson_parser_iterator

--- geophoto/geojson_parser.py
+++ geophoto/geojson_parser.py
@@ -10,6 +10,9 @@ class GeoJSONParser(object):
     def __init__(self):
         self._collections_dictionary = {}
 
+    def __iter__(self):
+        return iter(self._collections_dictionary.items())
+
     def add_feature(self, collection_title, lat, long, properties={}):
         '''
         
@@ -17,6 +20,10 @@ class GeoJSONParser(object):
         point = geojson.Point((lat, long))
         feature = geojson.Feature(geometry=point, properties=properties)
         if collection_title not in self._collections_dictionary:
-            self._collections_dictionary[collection_title] = [feature]
+            feature_collection = geojson.FeatureCollection(
+                features=[feature], 
+                title=collection_title
+                )
+            self._collections_dictionary[collection_title] = feature_collection
         else:
-            self._collections_dictionary[collection_title].append(feature)
+            self._collections_dictionary[collection_title]['features'].append(feature)
--- tests/test_geojson_parser.py
+++ tests/test_geojson_parser.py
@@ -1,5 +1,6 @@
 import unittest
 from geophoto.geojson_parser import GeoJSONParser
+import geojson
 
 
 class TestGeoJSONParser(unittest.TestCase):
@@ -15,7 +16,7 @@ class TestGeoJSONParser(unittest.TestCase):
             collection_title = test_title, lat=0, long=0, properties={}
         )
         self.assertTrue(test_title in geojson_parser._collections_dictionary)
-        self.assertEqual(1, len(geojson_parser._collections_dictionary[test_title]))
+        self.assertEqual(1, len(geojson_parser._collections_dictionary[test_title]['features']))
 
     def test_add_second_feature(self):
         geojson_parser = GeoJSONParser()
@@ -26,7 +27,7 @@ class TestGeoJSONParser(unittest.TestCase):
         geojson_parser.add_feature(
             collection_title = test_title, lat=0, long=0, properties={}
         )
-        self.assertEqual(2, len(geojson_parser._collections_dictionary[test_title]))
+        self.assertEqual(2, len(geojson_parser._collections_dictionary[test_title]['features']))
 
     def test_add_second_collection(self):
         geojson_parser = GeoJSONParser()
@@ -38,8 +39,41 @@ class TestGeoJSONParser(unittest.TestCase):
         geojson_parser.add_feature(
             collection_title = test_title2, lat=0, long=0, properties={}
         )
-        self.assertEqual(1, len(geojson_parser._collections_dictionary[test_title1]))
-        self.assertEqual(1, len(geojson_parser._collections_dictionary[test_title2]))
+        self.assertEqual(1, len(geojson_parser._collections_dictionary[test_title1]['features']))
+        self.assertEqual(1, len(geojson_parser._collections_dictionary[test_title2]['features']))
+
+# TODO - test invalid lat, long, properties, title: import geojson!!
+        
+    def test_geojson_parser_iterator(self):
+        geojson_parser = GeoJSONParser()
+
+        # test data
+        test_lat = 0
+        test_long = 0
+        test_title1 = 'Test_Title1'
+        test_title2 = 'Test_Title2'
+
+        # 
+        geojson_parser.add_feature(
+            collection_title = test_title1, lat=test_lat, long=test_long, properties={}
+        )
+        geojson_parser.add_feature(
+            collection_title = test_title2, lat=test_lat, long=test_long, properties={}
+        )
+        
+        # test geojson
+        test_point = geojson.Point((test_lat, test_long))
+        test_feature = geojson.Feature(geometry=test_point, properties={})
+        test_feature_collection = geojson.FeatureCollection(
+            features=[test_feature],
+            title=test_title1
+            )
+        
+        # iterator
+        it = iter(geojson_parser)
+        title, feature = next(it)
+        self.assertEqual(test_title1, title)
+        self.assertEqual(test_feature_collection, feature)
 
 
 
