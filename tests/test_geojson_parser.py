2f0f07cd5a913b8baba80699b5557107071790e8
Mike Bishop <mike@drinklist.co>
2024-02-22 12:35:19 +0000

    PASS test_init_geojson_parser_in_path

--- tests/test_geojson_parser.py
+++ tests/test_geojson_parser.py
@@ -3,7 +3,7 @@ from geophoto.geojson_parser import GeoJSONParser
 
 class TestGeoJSONParser(unittest.TestCase):
     
-    def test_init_geojson_parser(self):
+    def test_init_geojson_parser_in_path(self):
         in_path = 'tests/test_files/IMG_9729.jpg'
         geojson_parser = GeoJSONParser(in_path = in_path)
         self.assertEqual(in_path, geojson_parser.in_path)
