90d7cb9c6d8b0f2ceaaf13a916b55f5090c547fa
Mike Bishop <mike@drinklist.co>
2024-03-02 08:22:00 +0000

    PASS test_timer_status_finished

--- geophoto/timer.py
+++ geophoto/timer.py
@@ -5,7 +5,13 @@ class Timer(object):
 
     def __init__(self):
         self.elapsed_time = 0
+        self._in_progress = None
 
 
     def status(self):
-        print('Ready')
+        if self._in_progress is None:
+            print('Ready')
+        elif self._in_progress:
+            print('Running...')
+        elif not self._in_progress:
+            print(f'Finished in {self.elapsed_time} seconds')
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -13,10 +13,29 @@ class TestTimer(unittest.TestCase):
 
     def test_timer_status_ready(self):
         timer = Timer()
-
         f = io.StringIO()
         with redirect_stdout(f):
             timer.status()
         out = f.getvalue()
 
         self.assertEqual('Ready\n', out)
+
+    def test_timer_status_running(self):
+        timer = Timer()
+        timer._in_progress = True
+        f = io.StringIO()
+        with redirect_stdout(f):
+            timer.status()
+        out = f.getvalue()
+
+        self.assertEqual('Running...\n', out)
+
+    def test_timer_status_finished(self):
+        timer = Timer()
+        timer._in_progress = False
+        f = io.StringIO()
+        with redirect_stdout(f):
+            timer.status()
+        out = f.getvalue()
+
+        self.assertEqual('Finished in 0 seconds\n', out)
