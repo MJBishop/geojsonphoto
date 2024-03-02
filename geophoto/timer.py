d1532b80ef54fabd1bf1997986c9a9da0397b483
Mike Bishop <mike@drinklist.co>
2024-03-02 08:11:23 +0000

    PASS test_timer_status_ready

--- geophoto/timer.py
+++ geophoto/timer.py
@@ -5,3 +5,7 @@ class Timer(object):
 
     def __init__(self):
         self.elapsed_time = 0
+
+
+    def status(self):
+        print('Ready')
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,9 +1,22 @@
 import unittest
+import io
+from contextlib import redirect_stdout
 
 from geophoto.timer import Timer
+
 
 class TestTimer(unittest.TestCase):
 
     def test_timer_init(self):
         timer = Timer()
         self.assertEqual(0, timer.elapsed_time)
+
+    def test_timer_status_ready(self):
+        timer = Timer()
+
+        f = io.StringIO()
+        with redirect_stdout(f):
+            timer.status()
+        out = f.getvalue()
+
+        self.assertEqual('Ready\n', out)
