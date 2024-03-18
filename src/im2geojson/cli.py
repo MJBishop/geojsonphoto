94dc7058c8c76233cf1fb1b284e9712ac098730d
Mike Bishop <mike@drinklist.co>
2024-03-18 16:04:09 +0000

    PASS test_create_parser_prog

--- src/im2geojson/cli.py
+++ src/im2geojson/cli.py
@@ -6,6 +6,9 @@ import argparse
 
 
 def create_parser():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog='im2geojson',
+        description='Geojson from image metadata',
+        epilog='Text at the bottom of help')
 
     return parser
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,11 +7,36 @@ import unittest
 from im2geojson.cli import create_parser
 
 
-class TestCLI(unittest.TestCase):
+class TestParserCreate(unittest.TestCase):
 
     def test_create_parser(self):
         parser = create_parser()
         self.assertIsNotNone(parser)
+
+    def test_create_parser_prog(self):
+        parser = create_parser()
+        self.assertEqual('im2geojson', parser.prog)
+        self.assertEqual('Geojson from image metadata', parser.description)
+        # self.assertEqual('', parser.epilog)
+
+    
+
+
+# class TestParser(unittest.TestCase):
+
+#     def setUp(self):
+#         self.parser = create_parser()
+
+#     def test_parser_prog(self):
+#         parsed = self.parser.parse_args()
+#         self.assertEqual('im2geophoto', parsed)
+
+    # def test_parse(self):
+    #     parsed = self.parser.parse_args(['--something', 'test'])
+    #     with self.assertRaises:
+    #         self.assertEqual('test', parsed.something)
+
+    
 
 
 
